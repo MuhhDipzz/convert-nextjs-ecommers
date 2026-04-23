4b6d5924b83b4a8348ddb630321e8f79f00859e6
Dipzz-newbie <mnadhiftriyudo@gmail.com>
2026-04-23 20:10:50 +0700

    Update ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,25 @@
-'use client'
+"use client";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 export default function Page() {
   return (
     <div>
       <h1>test</h1>
       <Button className="bg-primary text-primary-foreground">click me</Button>
+      <Accordion type="single" collapsible>
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Item 1</AccordionTrigger>
+          <AccordionContent>Konten 1</AccordionContent>
+        </AccordionItem>
+      </Accordion>
+      <div className="bg-primary text-primary-foreground p-4">TEST PRIMARY</div>
     </div>
-  )
+  );
 }
